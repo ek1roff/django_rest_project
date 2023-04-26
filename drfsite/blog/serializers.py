8af8018021f4b858d57e31d1f0a4f9a076f0529c
ek1roff <kerlykov@gmail.com>
2023-04-26 14:45:40 +0700

    +serializer

--- drfsite/blog/serializers.py
+++ drfsite/blog/serializers.py
@@ -1,8 +1,37 @@
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from .models import Blog
 
 
-class BlogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Blog
-        fields = ('title', 'cat_id')
+# class BlogModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+
+class BlogSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    slug = serializers.CharField()
+    content = serializers.CharField()
+    photo = serializers.ImageField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+
+# def encode():
+#     model = BlogModel('Petr I', 'Content: Petr I')
+#     model_sr = BlogSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json, type(json), sep='\n')
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Petr I","content":"Content: Petr I"}')
+#     data = JSONParser().parse(stream)
+#     serializer = BlogSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
--- drfsite/blog/views.py
+++ drfsite/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+
 from .models import Blog
 from .serializers import BlogSerializer
 
@@ -8,6 +10,17 @@ class BlogAPIView(generics.ListAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
 
+    def get(self, request):
+        b = Blog.objects.all()
+        return Response({'posts': BlogSerializer(b, many=True).data})
+
+    def post(self, request):
+        post_new = Blog.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': BlogSerializer(post_new).data})
 
 def index(request):
     return render(request, 'blog/index.html', {'title': 'Главная страница'})
