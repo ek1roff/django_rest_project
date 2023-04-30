63f0e915c1fc946ffca680eb27f0306c3ad3463d
ek1roff <kerlykov@gmail.com>
2023-04-30 18:55:42 +0700

    +viewsets

--- drfsite/blog/views.py
+++ drfsite/blog/views.py
@@ -1,72 +1,31 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.response import Response
 
 from .models import Blog, Category
 from .serializers import BlogSerializer
 
 
-class BlogAPIList(generics.ListCreateAPIView):
+class BlogViewSet(viewsets.ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
 
 
-class BlogAPIUpdate(generics.UpdateAPIView):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
-
-
-class BlogAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
-
-
-# class BlogAPIView(generics.ListAPIView):
+# class BlogAPIList(generics.ListCreateAPIView):
 #     queryset = Blog.objects.all()
 #     serializer_class = BlogSerializer
 #
-#     def get(self, request):
-#         b = Blog.objects.all()
-#         return Response({'posts': BlogSerializer(b, many=True).data})
 #
-#     def post(self, request):
-#         serializer = BlogSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#
-#         return Response({'post': serializer.data})
-#
-#     def put(self, request, *args, **kwargs):
-#         pk = kwargs.get("pk", None)
-#         if not pk:
-#             return Response({"error": "Method PUT not allowed"})
-#
-#         try:
-#             instance = Blog.objects.get(pk=pk)
-#         except:
-#             return Response({"error": "Objects does not exist"})
-#
-#         serializer = BlogSerializer(data=request.data, instance=instance)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#
-#         return Response({"post": serializer.data})
-#
-#     def delete(self, request, *args, **kwargs):
-#         pk = kwargs.get("pk", None)
-#         if not pk:
-#             return Response({"error": "Method DELETE not allowed"})
-#
-#         try:
-#             instance = Blog.objects.get(pk=pk)
-#         except:
-#             return Response({"error": "Objects does not exist"})
-#
-#         instance.delete()
-#
-#         return Response({"post": "delete post " + str(pk)})
+# class BlogAPIUpdate(generics.UpdateAPIView):
+#     queryset = Blog.objects.all()
+#     serializer_class = BlogSerializer
 #
 #
+# class BlogAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Blog.objects.all()
+#     serializer_class = BlogSerializer
+
+
 def index(request):
     posts = Blog.objects.all()
     cats = Category.objects.all()
--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
-from django.urls import path
-from blog.views import (BlogAPIList,
-                        BlogAPIUpdate,
-                        BlogAPIDetailView,
+from django.urls import path, include
+from rest_framework import routers
+from blog.views import (BlogViewSet,
                         index)
+
+router = routers.SimpleRouter()
+router.register(r'blog', BlogViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/bloglist/', BlogAPIList.as_view()),
     path('', index, name='index'),
-    path('api/v1/bloglist/<int:pk>/', BlogAPIUpdate.as_view()),
-    path('api/v1/blogdetail/<int:pk>/', BlogAPIDetailView.as_view()),
+    path('api/v1/', include(router.urls)),
+    # path('api/v1/bloglist/', BlogViewSet.as_view({'get': 'list'})),
+    # path('api/v1/bloglist/<int:pk>/', BlogViewSet.as_view({'put': 'update'})),
+    # path('api/v1/blogdetail/<int:pk>/', BlogViewSet.as_view({'delete': 'destroy'})),
 ]
